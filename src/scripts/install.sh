226f11e1dd2eac68e4e74cf32264b1f692e3c149
Thomas Szteliga <ts@websafe.pl>
2015-11-12 15:51:47 +0100

    New install script.

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,13 +1,48 @@
 #!/bin/sh
 
-echo "Plugin Installed!"
+#
+set -e
 
-cd $DOCUMENT_ROOT # this directory
+# This directory (./scripts/)
+cd $DOCUMENT_ROOT
+
+# Switch to current plugin's root directory
 cd ..
-for dir in user reseller admin;
+
+#
+echo "<pre>"
+
+#
+for dir in scripts user;
 do
-    chmod 755 $dir/*
-    chown diradmin:diradmin $dir/*
+  chown -v diradmin:diradmin ${dir}
+  chown -v diradmin:diradmin ${dir}/*
+  chmod -v 755 ${dir}
+  chmod -v 755 ${dir}/*
 done
 
+#
+chown -v diradmin:diradmin \
+  LICENSE \
+  plugin.conf
+
+#
+find assets -exec \
+  chown -v diradmin:diradmin '{}' \;
+
+#
+find assets -type d -exec \
+  chmod -v 755 '{}' \;
+
+#
+find assets -type f -exec \
+  chmod -v 644 '{}' \;
+
+#
+echo "</pre>"
+
+#
+echo "<strong>Plugin Installed!</strong>"
+
+#
 exit 0
